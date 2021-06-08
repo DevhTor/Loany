7dea4849520a611efadc245f61218b0199fb76f5
Hector Aristy Castillo Mateo <hectoristy@hotmail.com>
2021-06-08 06:16:43 -0400

    Agregar archivos de proyecto.

---
+++ Loany/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Loany.Services;
+using Loany.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Loany
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
